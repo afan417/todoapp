6b4aa1acca3f14b6a7d169d45055835c99e2bc9b
Afan417 <batman.two@hotmail.com>
2024-01-14 18:50:29 +0100

    test 4

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -1,6 +1,8 @@
 import prisma from '../../prisma'
 
 export default async function handle(req, res) {
+  console.log('test')
+
   if (req.method == 'GET') {
     // fetch all todos.
     const todos = await prisma.todo.findMany({
